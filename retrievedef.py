ec2553740a3ea8b64ea302c8ee397e210f49dfda
Kalaborative <kalaborative94@gmail.com>
2020-11-17 12:26:31 -0600

    Initial commit

---
+++ retrievedef.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# opts = Options()
+# opts.add_argument('log-level=3')
+# opts.add_argument('--no-sandbox')
+# opts.add_argument('--headless')
+# browser = webdriver.Chrome(options=opts)
+
+def getDefinition(word, browser):
+    browser.get('https://en.wiktionary.org/wiki/{}'.format(word))
+    try:
+        orderedlist = browser.find_element_by_tag_name('ol').text
+    except Exception as e:
+        orderedlist = "Error: Could not find definition"
+    return orderedlist
